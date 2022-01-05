0674a6bf91ad8cf325b835dfde5173642092929f
Fabian Hüßler <fabian.huessler@st.ovgu.de>
2022-01-05 20:35:12 +0100

    examples/pio_blink: Add PIO blink example

---
+++ examples/pio_blink/main.c
@@ -0,0 +1,40 @@
+/*
+ * Copyright (C) 2023 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include "board.h"
+#include "periph/pio.h"
+#include "shell.h"
+
+pio_program_t pio_blink_export_program(void);
+
+int pio_blink_init(pio_program_t *blink,
+                   pio_t pio, pio_sm_t sm, const pio_program_t *pro,
+                   gpio_t pin);
+
+static const shell_command_t shell_commands[] = {
+    {NULL, NULL, NULL}
+};
+
+int main(void)
+{
+    pio_program_t blink = pio_blink_export_program();
+    pio_t pio;
+    pio_sm_t sm;
+    if (pio_alloc_program_sm_lock_any(&pio, &sm, &blink)) {
+        puts("No PIO resources available.");
+        return 1;
+    }
+    if (pio_blink_init(&blink, pio, sm, &blink, LED0_PIN)) {
+        puts("PIO blink initialization failed");
+        return 1;
+    }
+    pio_sm_start(pio, sm);
+
+    static char line_buf[SHELL_DEFAULT_BUFSIZE];
+    shell_run_forever(shell_commands, line_buf, SHELL_DEFAULT_BUFSIZE);
+}
---
+++ examples/pio_blink/rpx0xx_pio_blink/blink.c
@@ -0,0 +1,49 @@
+/*
+ * Copyright (C) 2023 Otto-von-Guericke-Universität Magdeburg
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+#include <errno.h>
+#include "pio/pio.h"
+#include "blink.hex.pio.h"
+
+static void pio_blink_init_pins(pio_t pio, pio_sm_t sm, gpio_t pin)
+{
+    pio_gpio_init_t init = {
+        .gpio_direction = 0x01,
+        .gpio_base = pin,
+        .gpio_count = 1
+    };
+    pio_sm_set_set_pins_init(pio, sm, &init);
+}
+
+pio_program_t pio_blink_export_program(void)
+{
+    return pio_blink_create_program();
+}
+
+int pio_blink_init(pio_program_t *blink,
+                   pio_t pio, pio_sm_t sm, const pio_program_t *pro,
+                   gpio_t pin)
+{
+    pio_instr_t instr[] = PIO_BLINK_PROGRAM;
+    pio_program_conf_t conf = PIO_BLINK_PROGRAM_CONF;
+    int ret;
+    if (pin >= 32) {
+        return -ENOTSUP;
+    }
+    if ((ret = pio_write_program(pio, pro, instr))) {
+        return ret;
+    }
+    if ((ret = pio_sm_init_common(pio, sm, pro, &conf))) {
+        return ret;
+    }
+    pio_sm_set_clkdiv(pio, sm, pio_sm_clkdiv(2000));
+    pio_blink_init_pins(pio, sm, pin);
+    pio_sm_restart(pio, sm);
+    *blink = *pro;
+    return 0;
+}
